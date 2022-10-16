2de7cbcfc528f06806190975bcad91cc24451e7f
Xulav <sulavthapachhetri@gmail.com>
2022-10-16 15:42:46 +0545

    adding unittest

--- src/validator.py
+++ src/validator.py
@@ -1,63 +1,67 @@
 class Validator:
-    def __init__(self,input_dictionary,validation_dictionary):
+    def __init__(self, input_dictionary, validation_dictionary):
         self.input_dictionary = input_dictionary
         self.validation_dictionary = validation_dictionary
 
-#     def type1(value,standard,input):
-#     if not type(value)==standard:
-#         print(f"The type of a {input} must be {standard} but is {type(value)}")
-#         return False
+    def type1(value, standard, input):
+        if not (type(value) == standard):
+            print(
+                f'The type of {input} must be {standard} but is {type(value)}')
+            return False
 
-# def minlength(value,standard,input):
-#     if not len(value)>=standard:
-#         print(f'The minimum length of a {input} must be {standard} but is {len(value)}')
-#         return False
+    def minlength(value, standard, input):
+        if not (len(value) >= standard):
+            print(
+                f'The minimum length of {input} must be {standard} but is {len(value)}')
+            return False
 
-# def maxlength(value,standard,input):
-#     if not len(value)<= standard:
-#         print(f'The maximum length of a {input} must be {standard} but is {len(value)}')
-#         return False
+    def maxlength(value, standard, input):
+        if not (len(value) <= standard):
+            print(
+                f'The maxmimum length of {input} must be {standard} but is {len(value)}')
+            return False
 
-# def isGreaterthan(value,standard,input):
-#     if not value>=standard:
-#         print(f'The {input} must be greater than {standard} but is {value}')
-#         return False
+    def minimum(value, standard, input):
+        if not (value >= standard):
+            print(
+                f'The minimum value of {input} must be {standard} but is {value}')
+            return False
 
-# def isLessthan(value,standard,input):
-#     if not value<=standard:
-#         print(f'The {input} must be less than {standard} but is {value}')
-#         return False
+    def maximum(value, standard, input):
+        if not (value <= standard):
+            print(
+                f'The maximum value of {input} must be {standard} but is {value}')
+            return False
 
-# def nestedfunction(value,standard,input):
-#     if(type(value)==list):
-#         for item in value:
-#             x = validator(item,standard)
-#             if x==False:
-#                 return False
-#     elif(type(value)==dict):
-#         x = validator(value,standard)
-#         if x==False:
-#             return False
+    def nestedlist(value, standard,input):
+        if (type(value) == list):
+            for item in value:
+                x = Validator.validator(item, standard)
+                if x == False:
+                    return False
+        elif (type(value) == dict):
+            x = Validator.validator(value, standard)
+            if x == False:
+                return False
 
-# def validator(dictionary, validationrule):
-#     if dictionary.keys()!=validationrule.keys():
-#        print("The keys in dictionary and Validation dictionary are different!")
-#        return False
-#     else:
-#         for key in validationrule:
-#             value = dictionary[key]
-#             rule = validationrule[key]
-#             for function in rule.keys():
-#                 if(function=='type' and type1(value,rule[function],key)==False):
-#                     return False
-#                 elif(function=='minlength' and minlength(value,rule[function],key)==False):
-#                     return False
-#                 elif(function=='maxlength' and maxlength(value,rule[function],key)==False):
-#                     return False
-#                 elif(function=='minimum' and isGreaterthan(value,rule[function],key)==False):
-#                     return False
-#                 elif(function=='max' and isLessthan(value,rule[function],key)==False):
-#                     return False
-#                 elif(function=='item_nesteddict' and nestedfunction(value,rule[function],key)==False):
-#                     return False
-#         return True
+    def validator(self):
+        if not self.input_dictionary.keys() == self.validation_dictionary.keys():
+            return False
+        else:
+            for key in self.validation_dictionary.keys():
+                value = self.input_dictionary.get(key)
+                rule = self.validation_dictionary.get(key)
+                for function in rule.keys():
+                    if (function == 'type' and Validator.type1(value, rule[function], key) == False):
+                        return False
+                    elif (function == 'minlength' and Validator.minlength(value, rule[function], key) == False):
+                        return False
+                    elif (function == 'maxlength' and Validator.maxlength(value, rule[function], key) == False):
+                        return False
+                    elif (function == 'minimum' and Validator.minimum(value, rule[function], key) == False):
+                        return False
+                    elif (function == 'maximum' and Validator.maximum(value, rule[function], key) == False):
+                        return False
+                    elif (function == 'item_nesteddict' and Validator.nestedfunction(value, rule[function]) == False):
+                        return False
+            return True
--- tests/unit/test_.py
+++ tests/unit/test_.py
@@ -1,18 +1,17 @@
-from typing import Type
 from unittest import TestCase
 from src.validator import Validator
-
+from unittest.mock import patch
 
 class TestValidator(TestCase):
 
     def setUp(self):
-        self.input_dictionary = {
+        input_dictionary = {
             "name": "Sulav",
             "age": 22,
             "city": "Syangja",
             "isEngineer": True
         }
-        self.validation_dictionary = {
+        validation_dictionary = {
             "name": {
                 "type": str,
                 'minlength': 4,
@@ -30,54 +29,92 @@ class TestValidator(TestCase):
                 'type': bool
             }
         }
-        self.validator = Validator(self.input_dictionary, self.validation_dictionary)
+        self.validator = Validator(input_dictionary, validation_dictionary)
 
     def test_initialization(self):
         self.assertEqual(isinstance(self.validator, Validator), True)
 
-        self.assertEqual(self.validator.input_dictionary, self.input_dictionary)
-        self.assertEqual(self.validator.validation_dictionary,self.validation_dictionary)
+        self.assertEqual(self.validator.input_dictionary, {
+            "name": "Sulav",
+            "age": 22,
+            "city": "Syangja",
+            "isEngineer": True
+        })
 
         # edge case for the dictionaries
-        self.assertRaises(TypeError, Validator,['name', 'age', 'city'] ,self.validation_dictionary)
-        self.assertRaises(TypeError, Validator,self.input_dictionary, (1,2,"int") )
-    
+        self.assertTrue(type(self.validator.validation_dictionary),
+                        type(self.validator.input_dictionary))
 
-    def test_validator(self):
-        #Checking if the keys of both dictionaries are equal or not
-        self.assertEqual(self.input_dictionary.keys(),self.validation_dictionary.keys())
-    
-
-    def test_type1(self):
-        test_value_inputdict = "Sulav"
-        test_value_valdict_key_value = str
-        test_x = "Hari"
-        test_y = int
-        #Checking the type of the input value with the validation
-        self.assertEqual(type(test_value_inputdict),test_value_valdict_key_value,"Should be a String")
-        #edge cases that may occur
-        # self.assertRaises(TypeError,lambda test_x,test_y: False if type(test_x)!= test_y else True,test_x,test_y)
+    def test_type(self):
+        test_value = 'Sulav'
+        test_rule = str
+        # check if the type of the input is acc to the standard
+        self.assertEqual(type(test_value), test_rule, True)
 
     def test_minlength(self):
-        test_value_inputdict = "Sulav"
-        test_value_valdict_key_value = 4
-        self.assertGreaterEqual(len(test_value_inputdict),test_value_valdict_key_value)
+        test_value = "Putalibazar"
+        test_min_length = 5
+        # Check if the length of the value is greater than or equal to test_min_length
+        self.assertGreaterEqual(len(test_value), test_min_length, True)
 
     def test_maxlength(self):
-        test_value_inputdict = "Sulav"
-        test_value_valdict_key_value = 10
-        self.assertLessEqual(len(test_value_inputdict),test_value_valdict_key_value)
+        test_value = "Putalibazar"
+        test_max_length = 25
+        # check if the length of the value is less than or equal to the test_max_length
+        self.assertLessEqual(len(test_value), test_max_length, True)
 
     def test_minimum(self):
-        test_value_inputdict = 22
-        test_value_valdict_key_value = 0
-        self.assertGreaterEqual(test_value_inputdict,test_value_valdict_key_value)
+        test_value = 22
+        test_min = 0
+        # check if the test_value is greater than or equal to the test_min
+        self.assertGreaterEqual(test_value, test_min, True)
 
-    def test_maximum(self):
-        test_value_inputdict = 22
-        test_value_valdict_key_value = 150
-        self.assertLessEqual(test_value_inputdict,test_value_valdict_key_value)
+    def test_maxmimum(self):
+        test_value = 22
+        test_max = 150
+        # check if the test_value is less than or equal to the test_min
+        self.assertLessEqual(test_value, test_max, True)
 
-    def test_item_nesteddict(self):
-        test_value_inputdict = [{}]
-        
+
+    @patch('src.validator.Validator.validator')
+    def test_nestedlist(self,mock_validator):
+        test_value = [
+            {'name': 'Faketown', 'population': 3},
+            {'name': 'Evergreen', 'population': 4}
+        ]
+        test_rule = {
+            'name': {
+                'type': str,
+                'minlength': 4,
+                'maxlength': 10
+            },
+            'population': {
+                'type': int,
+                'minimum': 0,
+            },
+        }
+        mock_validator.return_value = [False, True]
+        #Check whether the input value is of type list or dict ; if list, send the item of that list+ test_value as argument to the validator function
+        self.assertEqual(type(test_value), list,True)
+        self.assertEqual(self.validator.validator()[0],False)
+        self.assertEqual(self.validator.validator()[1],True)
+    
+    @patch('src.validator.Validator.type1', return_value = False)
+    @patch('src.validator.Validator.minlength', return_value = False)
+    @patch('src.validator.Validator.maxlength', return_value = False)
+    @patch('src.validator.Validator.minimum', return_value = False)
+    @patch('src.validator.Validator.maximum', return_value = False)
+    @patch('src.validator.Validator.nestedlist', return_value = False)
+    def test_validator(self,mock_type1,mock_minlength,mock_maxlength,mock_minimum,mock_maximum,mock_nestedlist):
+        # Checking if the keys of both dictionaries are equal or not
+        self.assertEqual(self.validator.input_dictionary.keys(),
+                         self.validator.validation_dictionary.keys(), True)
+
+        self.assertEqual(self.validator.type1(), False,"Should be False")
+        self.assertEqual(self.validator.minlength(), False,"Should be False")
+        self.assertEqual(self.validator.maxlength(), False,"Should be False")
+        self.assertEqual(self.validator.minimum(), False,"Should be False")
+        self.assertEqual(self.validator.maximum(), False,"Should be False")
+        self.assertEqual(self.validator.nestedlist(), False,"Should be False")
+    
+    
