d6edd4f32e26540a0c15f34c5b3259c4e69e8804
Xulav <sulavthapachhetri@gmail.com>
2022-10-16 16:41:11 +0545

    updated integrated

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -48,32 +48,37 @@ class TestValidator(TestCase):
     def test_type(self):
         test_value = 'Sulav'
         test_rule = str
+        input = "name"
         # check if the type of the input is acc to the standard
-        self.assertEqual(type(test_value), test_rule, True)
+        self.assertEqual(self.validator.type1(test_value,test_rule,input), None)
 
     def test_minlength(self):
         test_value = "Putalibazar"
         test_min_length = 5
+        input = 'city'
         # Check if the length of the value is greater than or equal to test_min_length
-        self.assertGreaterEqual(len(test_value), test_min_length, True)
+        self.assertGreaterEqual(self.validator.minlength(test_value,test_min_length,input), None)
 
     def test_maxlength(self):
         test_value = "Putalibazar"
         test_max_length = 25
+        input = 'city'
         # check if the length of the value is less than or equal to the test_max_length
-        self.assertLessEqual(len(test_value), test_max_length, True)
+        self.assertLessEqual(self.validator.minlength(test_value,test_max_length,input), None)
 
     def test_minimum(self):
         test_value = 22
         test_min = 0
+        input = "age"
         # check if the test_value is greater than or equal to the test_min
-        self.assertGreaterEqual(test_value, test_min, True)
+        self.assertGreaterEqual(self.validator.minimum(test_value,test_min,input),None)
 
     def test_maxmimum(self):
         test_value = 22
         test_max = 150
+        input = 'age'
         # check if the test_value is less than or equal to the test_min
-        self.assertLessEqual(test_value, test_max, True)
+        self.assertLessEqual(self.validator.minimum(test_value,test_max,input),None)
 
 
     def test_validator(self):
